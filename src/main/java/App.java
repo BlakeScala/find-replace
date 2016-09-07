d19ca0f77e6a058442317096ab55924f63d7a442
Blake Scala and Ewa Manek <ewa@>
2016-09-07 11:12:59 -0700

    write front end

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,21 @@ import java.io.Console;
 
 public class App {
   public static void main(String[] args) {
+    Console console = System.console();
 
+    System.out.println("Enter a sentence.");
+    String userSentence = console.readLine();
+
+    System.out.println("Enter a letter combination you want to replace");
+    String toReplace = console.readLine();
+
+    System.out.println("What would you like to replace it with?");
+    String replaceWith = console.readLine();
+
+    FindReplace findReplace = new FindReplace();
+
+    String newSentence = findReplace.replaceWord(userSentence, toReplace, replaceWith);
+
+    System.out.println(newSentence);
   }
 }
